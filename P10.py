7c17897af5b4e5272eb496f4ec3193cc7523adf0
AldoSantellano <97926833+AldoSantellano@users.noreply.github.com>
2023-11-21 21:57:07 -0600

    Práctica 10 v2
    Algunos cambios pequeños a la práctica.

--- P10.py
+++ P10.py
@@ -11,15 +11,15 @@ def open_file(path: str) -> str:
     return " ".join(content)
 
 db = pd.read_csv("VJVentas.csv", usecols=[1])
+#Para que pueda funcionar con normalidad este código se tiene que cambiar la ruta en donde se encuentra el archivo titulos.txt.
 np.savetxt(r'D:\GitHub\MineriaProyecto\titulos.txt', db.values, fmt='%s')
 
 
 all_words = ""
-frase = open_file("titulos.txt") # "hola a todos muchas  palabras palabras hola muchas hola hola hola palabras palabras hola muchas hola hola hola palabras palabras hola muchas hola hola hola palabras palabras hola muchas hola hola hola"
+frase = open_file("titulos.txt") 
 palabras = frase.rstrip().split(" ")
 
 Counter(" ".join(palabras).split()).most_common(10)
-# looping through all incidents and joining them to one text, to extract most common words
 for arg in palabras:
     tokens = arg.split()
     all_words += " ".join(tokens) + " "
@@ -29,14 +29,11 @@ wordcloud = WordCloud(
     background_color="white", min_font_size=5
 ).generate(all_words)
 
-# print(all_words)
-# plot the WordCloud image
 plt.close()
 plt.figure(figsize=(5, 5), facecolor=None)
 plt.imshow(wordcloud)
 plt.axis("off")
 plt.tight_layout(pad=0)
 
-# plt.show()
 plt.savefig("WordCloud.png")
 plt.close()
